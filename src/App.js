bff8f5d67586c496e641256abd5dcbec8d72b568
nicstewart0022 <nicstewart0022@gmail.com>
2022-01-17 12:27:49 -0700

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      Weather
+      <footer>
+        This project is created by Nicole Stewart is {""}
+        <a
+          href="https://github.com/nicstewart0022/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
